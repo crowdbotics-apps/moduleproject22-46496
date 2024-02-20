60d1f1baf7b0302e1540d420c95f076f5e72494a
Crowdbotics <engineering@crowdbotics.com>
2024-02-20 16:13:28 +0000

    Update store.

--- store/moduleprojectAPI/api.js
+++ store/moduleprojectAPI/api.js
@@ -14,6 +14,60 @@ function api_v1_login_create(payload) {
 function api_v1_signup_create(payload) {
   return moduleprojectAPI.post(`/api/v1/signup/`, payload)
 }
+function modules_two_factor_authentication_2fa_retrieve(payload) {
+  return moduleprojectAPI.get(`/modules/two-factor-authentication/2fa`)
+}
+function modules_two_factor_authentication_2fa_create(payload) {
+  return moduleprojectAPI.post(`/modules/two-factor-authentication/2fa`)
+}
+function modules_two_factor_authentication_twofactorauth_list(payload) {
+  return moduleprojectAPI.get(
+    `/modules/two-factor-authentication/twofactorauth/`
+  )
+}
+function modules_two_factor_authentication_twofactorauth_create(payload) {
+  return moduleprojectAPI.post(
+    `/modules/two-factor-authentication/twofactorauth/`,
+    payload
+  )
+}
+function modules_two_factor_authentication_twofactorauth_retrieve(payload) {
+  return moduleprojectAPI.get(
+    `/modules/two-factor-authentication/twofactorauth/${payload.id}/`
+  )
+}
+function modules_two_factor_authentication_twofactorauth_update(payload) {
+  return moduleprojectAPI.put(
+    `/modules/two-factor-authentication/twofactorauth/${payload.id}/`,
+    payload
+  )
+}
+function modules_two_factor_authentication_twofactorauth_partial_update(
+  payload
+) {
+  return moduleprojectAPI.patch(
+    `/modules/two-factor-authentication/twofactorauth/${payload.id}/`,
+    payload
+  )
+}
+function modules_two_factor_authentication_twofactorauth_destroy(payload) {
+  return moduleprojectAPI.delete(
+    `/modules/two-factor-authentication/twofactorauth/${payload.id}/`
+  )
+}
+function modules_two_factor_authentication_twofactorauth_send_otp_create(
+  payload
+) {
+  return moduleprojectAPI.post(
+    `/modules/two-factor-authentication/twofactorauth/send_otp/`,
+    payload
+  )
+}
+function modules_two_factor_authentication_verify_destroy(payload) {
+  return moduleprojectAPI.delete(
+    `/modules/two-factor-authentication/verify/${payload.id}/`
+  )
+}
 function rest_auth_login_create(payload) {
   return moduleprojectAPI.post(`/rest-auth/login/`, payload)
 }
@@ -51,6 +105,16 @@ export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  modules_two_factor_authentication_2fa_retrieve,
+  modules_two_factor_authentication_2fa_create,
+  modules_two_factor_authentication_twofactorauth_list,
+  modules_two_factor_authentication_twofactorauth_create,
+  modules_two_factor_authentication_twofactorauth_retrieve,
+  modules_two_factor_authentication_twofactorauth_update,
+  modules_two_factor_authentication_twofactorauth_partial_update,
+  modules_two_factor_authentication_twofactorauth_destroy,
+  modules_two_factor_authentication_twofactorauth_send_otp_create,
+  modules_two_factor_authentication_verify_destroy,
   rest_auth_login_create,
   rest_auth_logout_create,
   rest_auth_password_change_create,
